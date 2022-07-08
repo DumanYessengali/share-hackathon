1ef7b19aa46a46cd52c22b43f5e9dc5fdc568bfb
alimgaziyev.serzhan <alimgaziyevserzhan@gmail.com>
2022-07-08 13:01:16 +0600

    spring init

---
+++ src/test/java/kz/nis/share/ShareApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.nis.share;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
